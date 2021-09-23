772caf2fd3fe094cbf5cf40f98cb1b3a380028fa
Lee Jia Yi <jlee0237@student.monash.edu>
2021-09-24 01:53:25 +0800

    Update documentation in Player class

--- src/game/Player.java
+++ src/game/Player.java
@@ -13,7 +13,7 @@ public class Player extends Actor implements Soul {
 	private final Menu menu = new Menu();
 
 	/**
-	 * Constructor.
+	 * Constructor for the Player class.
 	 *
 	 * @param name        Name to call the player in the UI
 	 * @param displayChar Character to represent the player in the UI
@@ -28,23 +28,40 @@ public class Player extends Actor implements Soul {
 		this.addItemToInventory(new Broadsword());
 	}
 
+	/**
+	 * Returns a collection of the Actions that the otherActor can do to Player.
+	 *
+	 * @param actions    collection of possible Actions for this Actor
+	 * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+	 * @param map        the map containing the Actor
+	 * @param display    the I/O object to which messages may be written
+	 * @return A collection of Actions
+	 */
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
-		// Handle multi-turn Actions
 		if (lastAction.getNextAction() != null)
 			return lastAction.getNextAction();
 
 		display.println(name + " (" + hitPoints + "/" + maxHitPoints + ")" + ", holding " + getWeapon());
 		actions.add(new DrinkAction(this));
-		// return/print the console menu
 		return menu.showMenu(this, actions, display);
 	}
 
+	/**
+	 * Transfers the souls to another Soul's instance after Player is killed
+	 *
+	 * @param soulObject a target souls.
+	 */
 	@Override
 	public void transferSouls(Soul soulObject) {
 		//TODO: transfer Player's souls to another Soul's instance.
 	}
 
+	/**
+	 * Add points to the current Actor's hitpoint total.
+	 *
+	 * @param points number of hitpoints to add.
+	 */
 	@Override
 	public void heal(int points) {
 		double healPoints = points*0.4;
@@ -52,14 +69,29 @@ public class Player extends Actor implements Soul {
 		hitPoints = Math.min(hitPoints, maxHitPoints);
 	}
 
+	/**
+	 * Returns the maximum hitpoints for the player
+	 *
+	 * @return int that represent the player's maximum hitpoints
+	 */
 	public int getMaxHitPoints(){
 		return maxHitPoints;
 	}
 
+	/**
+	 * Returns the hitpoint for the player
+	 *
+	 * @return int that represents the player's hitpoints
+	 */
 	public int getHitPoints(){
 		return hitPoints;
 	}
 
+	/**
+	 * Gets the Estus Flask for the Player from the inventory
+	 *
+	 * @return the player's EstusFlask
+	 */
 	public EstusFlask getEstusFlask() {
 		for (Item item : inventory) {
 			if (item instanceof EstusFlask){
