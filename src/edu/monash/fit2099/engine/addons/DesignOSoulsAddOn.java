7df412451b6ec7be68fe73979ddcfcb1df63d172
AfridaJahin1020 <ajah0005@student.monash.edu>
2021-09-24 16:42:45 +0400

    Renamed DesignOSoulsAddOn back to original

--- src/edu/monash/fit2099/engine/addons/DesignOSoulsAddOn.java
+++ src/edu/monash/fit2099/engine/addons/DesignOSoulsAddOn.java
@@ -1,6 +1,6 @@
 package edu.monash.fit2099.engine.addons;
 
-import game.interfaces.Soul_I;
+import game.interfaces.Soul;
 
 /**
  * NOTE: Use this interface to add specific features according to the Assignment's scenario
@@ -12,5 +12,5 @@ public interface DesignOSoulsAddOn {
      * @return a reference to the current Actor/Ground/Item as type Soul,
      *         or null if this Actor/Ground/Item doesn't implement Soul.
      */
-    default Soul_I asSoul(){return this instanceof Soul_I ? (Soul_I) this : null;}
+    default Soul asSoul(){return this instanceof Soul ? (Soul) this : null;}
 }
