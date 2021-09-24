5f48859ad5fba72369fd38fb6282c92baf7a3e67
Lee Jia Yi <jlee0237@student.monash.edu>
2021-09-24 23:02:03 +0800

    Change Lord Of Cinder into an abstract class

--- src/game/LordOfCinder.java
+++ src/game/LordOfCinder.java
@@ -1,18 +1,43 @@
 package game;
 
 import edu.monash.fit2099.engine.*;
+import game.enums.Status;
+import game.interfaces.Behaviour;
+import game.interfaces.Soul;
+
+import java.util.ArrayList;
 
 /**
  * The boss of Design o' Souls
  * FIXME: This boss is Boring. It does nothing. You need to implement features here.
  * TODO: Could it be an abstract class? If so, why and how?
  */
-public class LordOfCinder extends Actor {
+public abstract class LordOfCinder extends Actor implements Soul{
+    private ArrayList<Behaviour> behaviours = new ArrayList<>();
+
     /**
      * Constructor.
      */
     public LordOfCinder(String name, char displayChar, int hitPoints) {
-        super(name, displayChar, hitPoints );
+        super(name, displayChar, hitPoints);
+    }
+
+    /**
+     * Returns a collection of the Actions that the otherActor can do to Skeleton.
+     *
+     * @param otherActor the Actor that might be performing attack
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
+     * @return A collection of Actions
+     */
+    @Override
+    public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
+        Actions actions = new Actions();
+        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            behaviours.add(0, new AttackBehaviour(otherActor, direction));
+            actions.add(new AttackAction(this,direction));
+        }
+        return actions;
     }
 
     /**
@@ -24,6 +49,12 @@ public class LordOfCinder extends Actor {
      */
     @Override
     public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+        for(game.interfaces.Behaviour Behaviour : behaviours) {
+            Action action = Behaviour.getAction(this, map);
+
+            if (action != null)
+                return action;
+        }
         return new DoNothingAction();
     }
 }
