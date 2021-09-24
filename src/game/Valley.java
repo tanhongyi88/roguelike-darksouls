fc624722eb70d15cd885e71d02b5fd8bd17bcdd2
htan0074 <htan0074@student.monash.edu>
2021-09-24 20:23:35 +0800

    make the valley to hurt player when he steps on it

--- src/game/Valley.java
+++ src/game/Valley.java
@@ -1,7 +1,9 @@
 package game;
 
+import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
 
 /**
  * The gorge or endless gap that is dangerous for the Player.
@@ -10,6 +12,21 @@ public class Valley extends Ground {
 
 	public Valley() {
 		super('+');
+	}
+
+	/**
+	 *
+	 * @param actor the Actor acting
+	 * @param location the current Location
+	 * @param direction the direction of the Ground from the Actor
+	 * @return blank actions
+	 */
+	@Override
+	public Actions allowableActions(Actor actor, Location location, String direction) {
+		if (location.getActor() == actor && location.getGround() instanceof Valley) {
+			actor.hurt(Integer.MAX_VALUE);
+		}
+		return new Actions();
 	}
 
 	/**
