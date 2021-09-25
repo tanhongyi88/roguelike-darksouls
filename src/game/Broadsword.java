3e95689ff506b68ce4025fc07bd8c21a1e8aa805
Lee Jia Yi <jlee0237@student.monash.edu>
2021-09-25 21:33:34 +0800

    Add Critical Strike to Broadsword and StormRuler

--- src/game/Broadsword.java
+++ src/game/Broadsword.java
@@ -1,11 +1,24 @@
 package game;
 
+import java.util.Random;
+
 public class Broadsword extends GameWeaponItem{
+    private static final int CRITICAL_STRIKE_PROBABILITY = 20;
     /**
      * Constructor.
      **/
 
     public Broadsword() {
         super("Broadsword", 'b', 30, "slashes", 80);
+    }
+
+    @Override
+    public int damage(){
+        Random random = new Random();
+
+        if(random.nextDouble()*100 <= CRITICAL_STRIKE_PROBABILITY){
+            return damage*2;
+        }
+        return damage;
     }
 }
--- src/game/StormRuler.java
+++ src/game/StormRuler.java
@@ -1,11 +1,23 @@
 package game;
 
-public class StormRuler extends GameWeaponItem{
+import java.util.Random;
 
+public class StormRuler extends GameWeaponItem{
+    private static final int CRITICAL_STRIKE_PROBABILITY = 20;
     /**
      * Constructor.
      */
     public StormRuler() {
         super("Storm Ruler", '7', 70, "hits", 60);
+    }
+
+    @Override
+    public int damage(){
+        Random random = new Random();
+
+        if(random.nextDouble()*100 <= CRITICAL_STRIKE_PROBABILITY){
+            return damage*2;
+        }
+        return damage;
     }
 }
