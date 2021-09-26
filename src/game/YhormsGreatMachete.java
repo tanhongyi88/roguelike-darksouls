661ec70d198a257ec84ad3d3a1bc8a326e5539ea
Lee Jia Yi <jlee0237@student.monash.edu>
2021-09-26 15:13:51 +0800

    Add Rage mode for Yhorm’s Great Machete

--- src/game/YhormsGreatMachete.java
+++ src/game/YhormsGreatMachete.java
@@ -2,6 +2,7 @@ package game;
 
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.WeaponAction;
+import game.enums.Abilities;
 
 public class YhormsGreatMachete extends GameWeaponItem{
 
@@ -10,6 +11,13 @@ public class YhormsGreatMachete extends GameWeaponItem{
      */
     public YhormsGreatMachete() {
         super("Yhorm's Great Machete", 'y', 95, "hit", 60);
+    }
+
+    public int chanceToHit(){
+        if(this.hasCapability(Abilities.EMBER_FORM)){
+            return this.hitRate + 30;
+        }
+        return this.hitRate;
     }
 
     @Override
--- src/game/enums/Abilities.java
+++ src/game/enums/Abilities.java
@@ -6,5 +6,6 @@ package game.enums;
 public enum Abilities {
     REST,
     RESURRECT,
-    SWAP
+    SWAP,
+    EMBER_FORM
 }
