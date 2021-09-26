5d64b5e5bd29b8af9892a721e1f47e8ccccbc2c5
Lee Jia Yi <jlee0237@student.monash.edu>
2021-09-26 13:45:34 +0800

    Revert changes to Estus Flask (not implementing Resettable)

--- src/game/EstusFlask.java
+++ src/game/EstusFlask.java
@@ -3,14 +3,13 @@ package game;
 import edu.monash.fit2099.engine.*;
 import game.interfaces.Resettable;
 
-public class EstusFlask extends Item implements Resettable {
+public class EstusFlask extends Item {
     private int numOfEstusFlask;
     private final int MAX_NUM_OF_ESTUS_FLASK = 3;
 
     public EstusFlask(String name) {
         super(name, 'e', false);
         this.numOfEstusFlask = MAX_NUM_OF_ESTUS_FLASK;
-        this.registerInstance();
     }
 
     public int getNumOfEstusFlask() {
@@ -23,15 +22,5 @@ public class EstusFlask extends Item implements Resettable {
 
     public void decrementNumberOfEstusFlask(){
         numOfEstusFlask -= 1;
-    }
-
-    @Override
-    public void resetInstance() {
-        this.numOfEstusFlask = MAX_NUM_OF_ESTUS_FLASK;
-    }
-
-    @Override
-    public boolean isExist() {
-        return true;
     }
 }
