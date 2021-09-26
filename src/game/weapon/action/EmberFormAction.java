fa085db84f7a883648fa4e0b6d26a6d4bd8bd7e2
htan0074 <htan0074@student.monash.edu>
2021-09-26 21:13:30 +0800

    ember form action is made to burning the surrounding dirts and increase the hit rate

--- src/game/weapon/action/EmberFormAction.java
+++ src/game/weapon/action/EmberFormAction.java
@@ -1,46 +1,55 @@
 package game.weapon.action;
 
 import edu.monash.fit2099.engine.*;
+import game.ground.BurningDirt;
 import game.weapon.YhormsGreatMachete;
 import game.player.Player;
+import game.ground.Dirt;
 
-public class EmberFormAction extends WeaponAction {
-    private YhormsGreatMachete greatMachete;
+public class EmberFormAction extends Action {
     private final static int BURN_DAMAGE = 25;
-    private Player player;
-    private int counter = 1;
+    private final YhormsGreatMachete greatMachete;
     private Location burningLocation;
     private boolean burnStart;
 
 
-    public EmberFormAction(WeaponItem greatMachete, Player player) {
-        super(greatMachete);
-        this.player = player;
+    /**
+     * Constructor
+     */
+    public EmberFormAction(YhormsGreatMachete greatMachete) {
+        this.greatMachete = greatMachete;
         this.burnStart = false;
     }
 
+    /**
+     * Burns the dirt at surroundings and damage the player if the player is on the burning ground.
+     * Raise the hitrate of Yhorms Great Machete from 60% to 90%.
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return  a string describing YhormTheGiant is engulfed with fire
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
+        
         if (!burnStart) {
             this.burningLocation = map.locationOf(actor);
+            this.greatMachete.increaseHitRateWhenEmberFore();
             this.burnStart = true;
         }
         if (burnStart) {
             for (Exit exit : this.burningLocation.getExits()) {
                 Location destination = exit.getDestination();
-                if (destination.getGround() instanceof Dirt && this.counter <= 3) {
-
-
+                if (destination.getGround() instanceof Dirt) {
+                    destination.setGround(new BurningDirt());
+                    destination.getGround().tick(destination);
                 }
                 if (destination.getActor() instanceof Player) {
                     destination.getActor().hurt(BURN_DAMAGE);
 
-
                 }
             }
         }
-
-        this.counter++;
         return menuDescription(actor);
     }
 
