621302fcb19a3b987d2f6d5a83dc234b44ded70f
htan0074 <htan0074@student.monash.edu>
2021-09-26 16:33:24 +0800

    update Soul interface method to allow token to be transferred correctly

--- src/game/TokenOfSouls.java
+++ src/game/TokenOfSouls.java
@@ -14,39 +14,43 @@ public class TokenOfSouls extends Item implements Soul{
      * @param name the name of this Item
      * @param displayChar the character to use to represent this item if it is on the ground
      * @param portable true if and only if the Item can be picked up
-     * @param soulsObject
+     * @param soulsObject soulsObject obtained to form this token (always from player)
      */
     public TokenOfSouls(String name, char displayChar, boolean portable, Soul soulsObject) {
         super(name, displayChar, portable);
-
+        soulsObject.transferSouls(this);
     }
 
     /**
      * Overriding getPickUpAction so that Souls can be retrieved using RetrieveSoulsAction
      * @param actor an actor that will interact with this item
-     * @return
+     * @return Retrieve action if token is portable (always portable) otherwise null
      */
     @Override
     public PickUpItemAction getPickUpAction(Actor actor) {
         if(portable)
-            return new RetrieveSoulsAction(this);
+            return new RetrieveSoulsAction(this, this.soulAmount);
 
         return null;
     }
 
     @Override
-    public void transferSouls(Souls soulObject) {
-
+    public void transferSouls(Soul soulObject) {
+        soulObject.addSouls(this.soulAmount);
+        this.soulAmount = 0;
     }
 
     @Override
-    public boolean addSouls(int soul_amount) {
-        return Soul.super.addSouls(soul_amount);
+    public boolean addSouls(int soulAmount) {
+        this.soulAmount += soulAmount;
+//        System.out.println("soulAmount: " + soulAmount);
+//        System.out.println("this.soulAmount: " + this.soulAmount);
+        return true;
     }
 
     @Override
-    public boolean subtractSouls(int soul_amount) {
-        return Soul.super.subtractSouls(soul_amount);
+    public boolean subtractSouls(int soulAmount) {
+        return Soul.super.subtractSouls(soulAmount);
     }
 }
 
